2b0e16c516e0a3d42436b2ce6379cd051d124b6a
josedcp20 <155150918+josedcp20@users.noreply.github.com>
2025-05-30 11:12:26 +0200

    Normalización database y noticias

--- database/renty.sql
+++ database/renty.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-05-2025 a las 10:33:04
+-- Tiempo de generación: 30-05-2025 a las 11:11:24
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -44,9 +44,8 @@ CREATE TABLE `alquileres` (
 CREATE TABLE `clientes` (
   `dni` varchar(9) NOT NULL,
   `nombre` varchar(50) DEFAULT NULL,
-  `apelidos` varchar(100) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `telefono` varchar(11) DEFAULT NULL
+  `apellidos` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -62,7 +61,18 @@ CREATE TABLE `coches` (
   `precio_alquiler` decimal(10,2) DEFAULT NULL,
   `disponible` tinyint(1) DEFAULT NULL,
   `id_sede` int(11) DEFAULT NULL,
-  `marca` varchar(20) NOT NULL
+  `id_marca` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `marcas`
+--
+
+CREATE TABLE `marcas` (
+  `id_marca` int(11) NOT NULL,
+  `nombre` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -75,6 +85,18 @@ CREATE TABLE `sedes` (
   `id_sede` int(11) NOT NULL,
   `nombre` varchar(100) DEFAULT NULL,
   `direccion` varchar(300) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `telefonos_clientes`
+--
+
+CREATE TABLE `telefonos_clientes` (
+  `id` int(11) NOT NULL,
+  `dni_cliente` varchar(9) DEFAULT NULL,
+  `telefono` varchar(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -100,13 +122,27 @@ ALTER TABLE `clientes`
 --
 ALTER TABLE `coches`
   ADD PRIMARY KEY (`matricula`),
-  ADD KEY `id_sede` (`id_sede`);
+  ADD KEY `id_sede` (`id_sede`),
+  ADD KEY `id_marca` (`id_marca`);
+
+--
+-- Indices de la tabla `marcas`
+--
+ALTER TABLE `marcas`
+  ADD PRIMARY KEY (`id_marca`);
 
 --
 -- Indices de la tabla `sedes`
 --
 ALTER TABLE `sedes`
   ADD PRIMARY KEY (`id_sede`);
+
+--
+-- Indices de la tabla `telefonos_clientes`
+--
+ALTER TABLE `telefonos_clientes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `dni_cliente` (`dni_cliente`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -119,10 +155,22 @@ ALTER TABLE `alquileres`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT de la tabla `marcas`
+--
+ALTER TABLE `marcas`
+  MODIFY `id_marca` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `sedes`
 --
 ALTER TABLE `sedes`
   MODIFY `id_sede` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `telefonos_clientes`
+--
+ALTER TABLE `telefonos_clientes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restricciones para tablas volcadas
@@ -139,7 +187,14 @@ ALTER TABLE `alquileres`
 -- Filtros para la tabla `coches`
 --
 ALTER TABLE `coches`
-  ADD CONSTRAINT `coches_ibfk_1` FOREIGN KEY (`id_sede`) REFERENCES `sedes` (`id_sede`);
+  ADD CONSTRAINT `coches_ibfk_1` FOREIGN KEY (`id_sede`) REFERENCES `sedes` (`id_sede`),
+  ADD CONSTRAINT `coches_ibfk_2` FOREIGN KEY (`id_marca`) REFERENCES `marcas` (`id_marca`);
+
+--
+-- Filtros para la tabla `telefonos_clientes`
+--
+ALTER TABLE `telefonos_clientes`
+  ADD CONSTRAINT `telefonos_clientes_ibfk_1` FOREIGN KEY (`dni_cliente`) REFERENCES `clientes` (`dni`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
